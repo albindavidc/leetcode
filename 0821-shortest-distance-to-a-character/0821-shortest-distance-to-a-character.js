f7a554494894d3b6dc55dc93847bf92b085dda2e
Albin David C <105530284+albindavidc@users.noreply.github.com>
2026-03-11 00:21:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.js
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.js
@@ -4,22 +4,19 @@
  * @return {number[]}
  */
 var shortestToChar = function(s, c) {
-    let result = new Array(s.length).fill(Infinity);
+    let result = Array(s.length).fill(Infinity);
+
     let prev = -Infinity;
-
-    // left → right
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === c) prev = i;
-        result[i] = i - prev;
+    for(let i = 0; i<s.length; i++){
+        if(s[i] === c) prev = i;
+        result[i] = i - prev
     }
 
-    // right → left
     prev = Infinity;
-
-    for (let i = s.length - 1; i >= 0; i--) {
-        if (s[i] === c) prev = i;
-        result[i] = Math.min(result[i], prev - i);
+    for(let i = 0; i<s.length; i++){
+        if(s[i] ==c ) prev = i;
+        result[i] = Math.min(result[i], prev-i)
     }
 
-    return result;
+    return result
 };
