ace3b61caf119bab845e4ab6409d562449f1e5c9
Albin David C <105530284+albindavidc@users.noreply.github.com>
2026-04-16 18:15:44 +0530

    Time: 0 ms (100%), Space: 53.8 MB (50%) - LeetHub

---
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} forts
+ * @return {number}
+ */
+var captureForts = function (forts) {
+
+    let result = 0;
+
+    let count = 0;
+    let last = null;
+
+
+   for(let i = 0; i<forts.length; i++){
+    if(forts[i] === 0){
+        count++;
+    }else{
+        if(last !== null && forts[i] !== last){
+            result = Math.max(result, count);
+        }
+
+        count = 0
+        last = forts[i]
+    }
+   }
+
+    return result
+};
