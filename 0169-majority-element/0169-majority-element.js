eabf5692d0a1c5cbb840023841439f64d9406011
Albin David C <105530284+albindavidc@users.noreply.github.com>
2026-06-03 13:33:15 +0530

    Time: 6 ms (35.38%), Space: 55.7 MB (90.31%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -2,37 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function (nums) {
-    if (!nums) return null
-    return majorityElementRec(nums, 0, nums.length - 1);
-};
+var majorityElement = function(nums) {
+    let store = new Map();
+    let larger = 0;
+    let largerCount = 0;
 
-function majorityElementRec(arr, start, end) {
-    if (start === end) {
-        return arr[start]
-    }
+    for(let i = 0; i<nums.length; i++){
+        if(store.set(nums[i], (store.get(nums[i]) || 0) + 1));
 
-
-    let mid = Math.floor((start + end) / 2);
-    let leftMajority = majorityElementRec(arr, start, mid);
-    let rightMajority = majorityElementRec(arr, mid + 1, end);
-
-    if (leftMajority === rightMajority) {
-        return leftMajority;
-    }
-
-    let leftCount = countInRange(arr, leftMajority, start, end);
-    let rightCount = countInRange(arr, rightMajority, start, end);
-
-    return leftCount > rightCount ? leftMajority : rightMajority;
-}
-
-function countInRange(arr, nums, start, end) {
-    let count = 0;
-    for (let i = start; i <= end; i++) {
-        if (arr[i] === nums) {
-            count++;
+        if(largerCount < store.get(nums[i])){
+            larger = nums[i];
+            largerCount = store.get(nums[i])
         }
     }
-    return count;
-}
+
+    return larger
+};
