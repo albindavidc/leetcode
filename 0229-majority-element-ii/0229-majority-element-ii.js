c17ca70273da111256e11510611e64f92d912067
Albin David C <105530284+albindavidc@users.noreply.github.com>
2026-08-13 15:38:01 +0530

    Time: 5 ms (69.29%), Space: 56.9 MB (79.62%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement = function (nums) {
+    let count = 1;
+
+    nums = nums.sort((a,b) => a-b)
+
+    let result = []
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (i < nums.length && nums[i - 1] === nums[i]) {
+            count++;
+        } else {
+            if (count > Math.floor(nums.length / 3)) {
+                result.push(nums[i - 1])
+            }
+            count = 1
+        }
+
+    }
+
+    return result
+};
