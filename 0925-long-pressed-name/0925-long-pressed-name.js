66833c7f59f4e92c5f45c5448223694565ccc388
Albin David C <105530284+albindavidc@users.noreply.github.com>
2026-09-25 21:59:12 +0530

    Time: 0 ms (100%), Space: 53.6 MB (67.86%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function (name, typed) {
+    let ptrA = 0;
+    let ptrB = 0;
+
+    while (ptrB < typed.length) {
+        if (ptrA < name.length && name[ptrA] === typed[ptrB]) {
+            ptrA++;
+        }
+        else if(typed[ptrB] !== typed[ptrB-1]) {
+            return false;
+        }
+
+        ptrB++
+    }
+
+    return ptrA === name.length;
+};
