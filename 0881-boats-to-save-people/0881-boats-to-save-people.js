a2522dbdb9c61aee30aa6112e5b0652b7e40100e
Albin David C <105530284+albindavidc@users.noreply.github.com>
2026-09-26 22:01:05 +0530

    Time: 70 ms (34.54%), Space: 62.8 MB (79.38%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function (people, limit) {
+    people = people.sort((a, b) => a - b);
+
+    let result = 0;
+    let left = 0;
+    let right = people.length - 1;
+
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) {
+            left++;
+        }
+
+        right--
+        result++
+    }
+
+    return result
+};
